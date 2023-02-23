098b2c49aff45ae3bc77931fdccca4240162f153
Ballademager <118718923+Ballademager@users.noreply.github.com>
2023-02-23 10:37:03 +0100

    oprydning

--- animationForkertnavn.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", sidenVises);
-
-function sidenVises() {
-  console.log("sidenVises");
-  document.querySelector(".heart1-container").classList.add("float1");
-  document.querySelector(".heart2-container").classList.add("float2");
-  document.querySelector(".heart3-container").classList.add("float3");
-}
